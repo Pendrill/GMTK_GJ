903423b1d2317019560cd5524bc1a7e01f1bf018
Aleksei Waddington <amw694@nyu.edu>
2017-07-15 13:57:32 -0400

    player appears, then enemy shows up

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
     public float maxHealth, currentHealth;
-    public enum GameState { appear, attack, die};
+    public enum GameState { wait, appear, attack, die};
     public GameState currentState;
     float lastStateChange = 0.0f;
+    float time;
+
+    SpriteRenderer enemyRenderer;
+    float alpha;
 	// Use this for initialization
 	void Start () {
         maxHealth = 100.0f;
         currentHealth = maxHealth;
+        enemyRenderer = GetComponent<SpriteRenderer>();
+        //setCurrentState(GameState.appear);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Debug.Log(currentState);
         switch (currentState)
         {
+            case GameState.wait:
+                break;
             case GameState.appear:
-
+                time += Time.deltaTime*2;
+                Color tmp = enemyRenderer.color;
+                alpha = Mathf.Lerp(0.0f, 1.0f, time);
+                tmp.a = alpha;
+                enemyRenderer.color = tmp;
                 break;
             case GameState.attack:
 
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -8,33 +8,48 @@ public class PlayerManager : MonoBehaviour {
 
     //have access to the gameManager
     GameManager theGameManager;
+    EnemyManager theEnemyManager;
 
     //List of states the player character can access
-    public enum GameState { EnterScene, Fight, LeaveScene, Die}
+    public enum GameState { EnterScene, Fight, LeaveScene, Die, Wait}
     //keep track of the player's current state
     public GameState currentState;
 
     //keep track of time spent in the current scene
     float lastStateChange = 0.0f;
 
+    public Vector3 offScreenStartingPos, FightingPos, ExitPos;
+
 	// Use this for initialization
 	void Start () {
         maxHealth = 100.0f;
         currentHealth = maxHealth;
-        theGameManager = FindObjectOfType<GameManager>();
+        //theGameManager = FindObjectOfType<GameManager>();
+        offScreenStartingPos = new Vector3(-5, -6, 0);
+        FightingPos = new Vector3(-5, -1, 0);
+        ExitPos = new Vector3(-5, 11, 0);
+        setCurrentState(GameState.EnterScene);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         switch (currentState)
         {
+            case GameState.Wait:
+                break;
             case GameState.EnterScene:
-
-                
+                transform.position = Vector3.Lerp(offScreenStartingPos, FightingPos, Time.time/2);
+                transform.localScale = Vector3.Lerp(new Vector3(30, 30, 30), new Vector3(15, 15, 15), Time.time/2);
+                if(getStateElapsed() > 2.0f)
+                {
+                    theEnemyManager = FindObjectOfType<EnemyManager>();
+                    theEnemyManager.setCurrentState(EnemyManager.GameState.appear);
+                    setCurrentState(GameState.Fight);
+                }
                 break;
 
             case GameState.Fight:
-
+                Debug.Log("You have now entered the fighting state");
                 break;
 
             case GameState.LeaveScene:
@@ -43,7 +58,7 @@ public class PlayerManager : MonoBehaviour {
 
             case GameState.Die:
 
-                theGameManager.setCurrentState(GameManager.GameState.GameOverSequence);
+                //theGameManager.setCurrentState(GameManager.GameState.GameOverSequence);
                 break;
         }
 	}
