41a88e18b21b0d91a7731f9dbb605007a18ccf5f
Aleksei Waddington <amw694@nyu.edu>
2017-07-15 12:55:12 -0400

    Basic skeleton for state machines set up

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -6,13 +6,37 @@ public class EnemyManager : MonoBehaviour {
     public float maxHealth, currentHealth;
     public enum GameState { appear, attack, die};
     public GameState currentState;
+    float lastStateChange = 0.0f;
 	// Use this for initialization
 	void Start () {
-		
+        maxHealth = 100.0f;
+        currentHealth = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (currentState)
+        {
+            case GameState.appear:
+
+                break;
+            case GameState.attack:
+
+                break;
+            case GameState.die:
+
+                break;
+        }
 	}
+
+    public void setCurrentState(GameState state)
+    {
+        currentState = state;
+        lastStateChange = Time.time;
+    }
+
+    float getElapsedTime()
+    {
+        return Time.time - lastStateChange;
+    }
 }
--- Assets/Scripts/Managers/ShopkeeperManager.cs
+++ Assets/Scripts/Managers/ShopkeeperManager.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopkeeperManager : MonoBehaviour {
-
+    public float maxHealth, currentHealth;
+    public enum GameState { appear, barter, attack, leave, die };
+    public GameState currentState;
+    float lastStateChange = 0.0f;
 	// Use this for initialization
 	void Start () {
-		
+        maxHealth = 100.0f;
+        currentHealth = maxHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (currentState)
+        {
+            case GameState.appear:
+
+                break;
+            case GameState.barter:
+
+                break;
+            case GameState.attack:
+
+                break;
+            case GameState.leave:
+
+                break;
+            case GameState.die:
+
+                break;
+        }
 	}
+
+    public void getCurrentState(GameState state)
+    {
+        currentState = state;
+        lastStateChange = Time.time;
+    }
+    
+    float getElapsedTime()
+    {
+        return Time.time - lastStateChange;
+    }
 }
