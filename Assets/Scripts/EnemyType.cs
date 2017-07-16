f5be270f8e9b697898f86ace899c375ea77f3f78
Aleksei Waddington <amw694@nyu.edu>
2017-07-16 00:32:30 -0400

    Started work on damging enemies. Enemy now posess one material that will drop upon death

---
+++ Assets/Scripts/EnemyType.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType : MonoBehaviour {
+    public string monsterType;
+    public Item drop;
+	// Use this for initialization
+	void Start () {
+        drop = (Item)Random.Range(1, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private void OnCollisionEnter2D(Collision2D bomb)
+    {
+        BombController theBomb = bomb.gameObject.GetComponent<BombController>();
+        if (monsterType.Trim().Equals("Fire".Trim()))
+        {
+            if(theBomb.AFFINITY == Element.Water)
+            {
+
+            }else if(theBomb.AFFINITY == Element.Earth)
+            {
+
+            }else
+            {
+
+            }
+        }
+        else if (monsterType.Trim().Equals("Water".Trim()))
+        {
+            if(theBomb.AFFINITY == Element.Air)
+            {
+
+            }else if(theBomb.AFFINITY == Element.Fire)
+            {
+
+            }else
+            {
+
+            }
+        }
+        else if (monsterType.Trim().Equals("Air".Trim()))
+        {
+            if(theBomb.AFFINITY == Element.Earth)
+            {
+                
+            }else if(theBomb.AFFINITY == Element.Water)
+            {
+
+            }else
+            {
+
+            }
+        }
+        else if (monsterType.Trim().Equals("Earth".Trim()))
+        {
+            if(theBomb.AFFINITY == Element.Fire)
+            {
+
+            }else if(theBomb.AFFINITY == Element.Air)
+            {
+
+            }else
+            {
+
+            }
+        }
+    }
+}
